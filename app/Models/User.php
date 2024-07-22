5cc79066330797559ab83db8fd79cd0d8521cdad
Crescian <lcoryed@gmail.com>
2024-07-22 14:47:19 +0800

    dev/crescian/push_to_database

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
         'first_name',
         'last_name',
         'initial',
-        'posiion',
+        'position',
         'company',
         'middle_initial',
     ];
